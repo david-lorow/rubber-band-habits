39fada3f19a2b5fad975ffdc188253e9cfd264c3
David <david.lorow@gmail.com>
2024-11-08 14:39:31 -0500

    Initial Commit

---
+++ Rubber Band Habits/Rubber_Band_HabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rubber_Band_HabitsApp.swift
+//  Rubber Band Habits
+//
+//  Created by David Lorow on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Rubber_Band_HabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Rubber Band HabitsTests/Rubber_Band_HabitsTests.swift
@@ -0,0 +1,17 @@
+//
+//  Rubber_Band_HabitsTests.swift
+//  Rubber Band HabitsTests
+//
+//  Created by David Lorow on 11/8/24.
+//
+
+import Testing
+@testable import Rubber_Band_Habits
+
+struct Rubber_Band_HabitsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
